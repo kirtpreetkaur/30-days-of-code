602a13e4aaacbd859776da785f2b048c29b07f49
KIRTPREET KAUR <84916234+kirtpreetkaur@users.noreply.github.com>
2022-04-26 17:00:36 +0530

    Update and rename BIGARRAY.cp to BIGARRAY.cpp

--- KIRTPREET KAUR/BIGARRAY.cpp
+++ KIRTPREET KAUR/BIGARRAY.cpp
@@ -9,7 +9,9 @@ while(t--) {
   ll n,s;
   cin>>n>>s;
   ll pos = (n*(n+1)/2)-s;
-    if(pos>=1&&pos<=n) cout<<pos<<endl; else cout<<"-1"<<endl;
+    if(pos>=1&&pos<=n) cout<<pos<<endl; 
+    else 
+    cout<<"-1"<<endl;
   }
  return 0; 
     
