b73ec68b878b2788eb4de62254004f1e1a8c5e86
KIRTPREET KAUR <84916234+kirtpreetkaur@users.noreply.github.com>
2022-04-28 20:27:03 +0530

    Create DAY-2 TALLER.cpp

---
+++ KIRTPREET KAUR/DAY-2 TALLER.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t, a_age, b_age;
+    100<a_age, b_age<200;
+    cin >> t;
+    while (t--){
+        cin >> a_age;
+        cin >> b_age;
+        if (a_age>b_age)
+        cout<<"A"<<endl;
+        else
+        cout<<"B"<<endl;
+    }
+	return 0;
+}
