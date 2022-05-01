b1939c24e288548f98fecb229d05b2fcef01c280
KIRTPREET KAUR <84916234+kirtpreetkaur@users.noreply.github.com>
2022-05-01 20:47:19 +0530

    Create DAY-10 XLSQUARE.cpp

---
+++ KIRTPREET KAUR/DAY-10 XLSQUARE.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,a;
+	    cin>>n>>a;
+	    int side;
+	    side=a*(int)sqrt(n);
+	    cout<<side<<endl;
+	}
+	return 0;
+}
