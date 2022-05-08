f6a949193957f63580e950379b68cfea919c1e14
KIRTPREET KAUR <84916234+kirtpreetkaur@users.noreply.github.com>
2022-05-08 23:55:34 +0530

    Create DAY- 15 DIAGMOVE.cpp

---
+++ KIRTPREET KAUR/DAY- 15 DIAGMOVE.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    if((x+y)%2==0){
+	        cout<<"YES"<<endl;
+	    }
+	    else{cout<<"NO"<<endl;}
+	}
+	return 0;
+}
