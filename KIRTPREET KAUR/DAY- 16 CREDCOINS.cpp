1522eb0d52225cdb2e99ae2cfd81239e8eb73f9c
KIRTPREET KAUR <84916234+kirtpreetkaur@users.noreply.github.com>
2022-05-09 23:10:42 +0530

    Create DAY- 16 CREDCOINS.cpp

---
+++ KIRTPREET KAUR/DAY- 16 CREDCOINS.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x, y, t, z;
+	cin>>t;
+	while(t--){
+	cin>>x>>y;
+	z=x*y;
+	1<=x;
+	y<=1000;
+	if(z>=100){
+	    cout<<z/100<<"\n";
+	}
+	else{
+	    cout<<"0"<<"\n";
+	}
+	}
+	return 0;
+}
