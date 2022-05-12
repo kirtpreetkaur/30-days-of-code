73ba0b0cd162fe234e4ea4d94526d1938169847b
KIRTPREET KAUR <84916234+kirtpreetkaur@users.noreply.github.com>
2022-05-13 01:36:29 +0530

    Create DAY- 19 SUBSCRIBE.cpp

---
+++ KIRTPREET KAUR/DAY- 19 SUBSCRIBE.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while (t--){
+        int s;
+        cin>>s;
+        if(s>30)
+        cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+	return 0;
+}
