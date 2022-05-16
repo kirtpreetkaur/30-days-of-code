e6db8750b73bef87e9d16c01d6b0c36c1a547686
KIRTPREET KAUR <84916234+kirtpreetkaur@users.noreply.github.com>
2022-05-17 01:23:00 +0530

    Create DAY- 23 COURSEREG.cpp

---
+++ KIRTPREET KAUR/DAY- 23 COURSEREG.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int t;
+	cin>>t;
+	while (t--){
+	    int n, m, k;
+	    cin>>n>>m>>k;
+	    if((m-k)>=n)
+	    cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+	return 0;
+}
