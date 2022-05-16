57fb5b3fc731c14644fd0d06456a6ffdabc66e15
KIRTPREET KAUR <84916234+kirtpreetkaur@users.noreply.github.com>
2022-05-17 01:32:06 +0530

    Create DAY- 23 BATTERYLOW.cpp

---
+++ KIRTPREET KAUR/DAY- 23 BATTERYLOW.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x<=15)
+	    cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+	return 0;
+}
