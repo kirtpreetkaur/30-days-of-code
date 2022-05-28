d9882b6a802d2caba030b5f7f82f3d0450057d0a
KIRTPREET KAUR <84916234+kirtpreetkaur@users.noreply.github.com>
2022-05-28 21:29:00 +0530

    Create DAY- 33 ATM.cpp

---
+++ KIRTPREET KAUR/DAY- 33 ATM.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<iostream>
+using namespace std;
+int main(){
+	int x;
+	float y;
+cin>>x>>y;
+  if(x%5==0){
+      y= (y-x-(0.50));
+  }
+ printf("%.2f", y);
+	return 0;
+}
