dc129777723e49d7062b00f95718219236ff1dc3
KIRTPREET KAUR <84916234+kirtpreetkaur@users.noreply.github.com>
2022-05-29 11:19:47 +0530

    Create DAY- 34 CHEFRACES.cpp

---
+++ KIRTPREET KAUR/DAY- 34 CHEFRACES.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x, y, a, b;
+	    cin>>x>>y>>a>>b;
+	    if((x==a || x==b) && (y==a || y==b)) cout<<"0"<<endl;
+	  else if ((x==a || x==b && y!=a &&y!=b)||(y==a || y==b && x!=a && x!=b))
+	    cout<<"1"<<endl;
+	    else cout<<"2"<<endl;
+	}
+	return 0;
+}
