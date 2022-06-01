219b262473a72854fa34209436c1be66034dab2c
KIRTPREET KAUR <84916234+kirtpreetkaur@users.noreply.github.com>
2022-06-02 00:48:24 +0530

    Create DAY- 38 SNAPE.cpp

---
+++ KIRTPREET KAUR/DAY- 38 SNAPE.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int b, ls;
+        cin>>b>>ls;
+        cout<<sqrt(ls*ls - b*b)<<" "<<sqrt(ls*ls + b*b)<<endl;
+    }
+    return 0;
+}
