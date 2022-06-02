d1b2fe0ec960dede81765dfc8c35cf3a5e66dfff
KIRTPREET KAUR <84916234+kirtpreetkaur@users.noreply.github.com>
2022-06-03 02:00:28 +0530

    Create DAY- 39 CCHOCOLATES.cpp

---
+++ KIRTPREET KAUR/DAY- 39 CCHOCOLATES.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x, y, z, a;
+	    cin>>x>>y>>z;
+	    cout<<((5*x)+(10*y))/z<<endl;
+	}
+	return 0;
+}
