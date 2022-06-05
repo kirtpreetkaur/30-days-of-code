3d5cde55ff72703d153b1360bad44278fcfb4a00
KIRTPREET KAUR <84916234+kirtpreetkaur@users.noreply.github.com>
2022-06-06 03:41:10 +0530

    Create DAY- 41 EITA.cpp

---
+++ KIRTPREET KAUR/DAY- 41 EITA.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int d, x, y, z;
+	    cin>>d>>x>>y>>z;
+	    cout<<max((x*7),((y*d)+(z*(7-d))))<<endl;
+	}
+	return 0;
+}
