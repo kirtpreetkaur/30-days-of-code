bb8f44fa360c68432352679987223e3d9525c22e
KIRTPREET KAUR <84916234+kirtpreetkaur@users.noreply.github.com>
2022-06-06 17:09:33 +0530

    Create DAY- 42 CHEFTRANS.cpp

---
+++ KIRTPREET KAUR/DAY- 42 CHEFTRANS.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x, y, z;
+	    cin>>x>>y>>z;
+	    if((x+y)>z) cout<<"TRAIN"<<endl;
+	    else if ((x+y)<z) cout<<"PLANEBUS"<<endl;
+	    else cout<<"EQUAL"<<endl;
+	}
+	return 0;
+}
