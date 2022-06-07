07cf61207db33e756a351ddbb66110de73c94952
KIRTPREET KAUR <84916234+kirtpreetkaur@users.noreply.github.com>
2022-06-08 02:05:31 +0530

    Create DAY- 43 PROBCAT .cpp

---
+++ KIRTPREET KAUR/DAY- 43 PROBCAT .cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x>=1 && x<100) cout<<"Easy"<<endl;
+	    else if (x>=200 && x<=300) cout<<"Hard"<<endl;
+	    else cout<<"Medium"<<endl;
+	}
+	return 0;
+}
