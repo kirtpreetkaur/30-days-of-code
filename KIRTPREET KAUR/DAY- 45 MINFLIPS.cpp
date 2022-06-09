60b1a117f41725326f3051d898a33feebc37c5a6
KIRTPREET KAUR <84916234+kirtpreetkaur@users.noreply.github.com>
2022-06-10 00:46:52 +0530

    Create DAY- 45 MINFLIPS.cpp

---
+++ KIRTPREET KAUR/DAY- 45 MINFLIPS.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int sum=0; 
+        for(int i=1;i<=N;i++){ 
+            int a;
+            cin>>a;
+            sum+=A;
+}
+if(abs (sum)%2!=0) cout<<"-1"<<endl;
+else cout<<abs (sum)/2<<endl;
+}
+return 0;
+}
