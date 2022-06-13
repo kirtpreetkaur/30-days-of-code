05ce23b074841dfa27130a01a5dde75b21cc63ad
KIRTPREET KAUR <84916234+kirtpreetkaur@users.noreply.github.com>
2022-06-14 02:42:13 +0530

    Create DAY- 47 ALTERADD.cpp

---
+++ KIRTPREET KAUR/DAY- 47 ALTERADD.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a, b;
+	    cin>>a>>b;
+	    if((b-a)%3==1||(b-a)%3==0) cout<<"Yes"<<endl;
+	    else cout<<"No"<<endl;
+	}
+	return 0;
+}
